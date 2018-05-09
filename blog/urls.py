29770aedda8a1227d574d2e4b644dddede40ab5d
Taewon Song <crazytb@gmail.com>
2018-05-10 00:10:21 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    # re_path(r'^$', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    # path('admin/', admin.site.urls),
+]
